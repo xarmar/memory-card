a94230ee2b8202f01b66191bc7c5352290f15799
Xavier Marques <xavierpmarques@gmail.com>
2021-09-30 23:14:21 +0100

    Add characterFactory.tsx to create Characters

---
+++ src/factoryFunctions/characterFactory.tsx
@@ -0,0 +1,11 @@
+import { Char } from "../types/customTypes"
+
+const createCharacter = (charName: string, pictureUrl: string): Char => {
+    return ({
+        name: charName,
+        picture: pictureUrl,
+        }
+    )
+}
+
+export default createCharacter
