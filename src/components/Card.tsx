c9a03ee18f381e6058759edac7750197d55f2d26
Xavier Marques <xavierpmarques@gmail.com>
2021-10-02 00:52:48 +0100

    Update Card.tsx Component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,45 @@
-import { Char } from "../types/customTypes"
+import { Char } from "../types/customTypes";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import styled from "@emotion/styled";
+
+type bgImageProps = {
+  picture: any
+}
+
+const CardContainer = styled(Grid)`
+  background-color: whitesmoke;
+`;
+
+const BackgroundImageDiv = styled.div<bgImageProps>`
+  background-image: url(${props => props.picture});
+  background-size: cover;
+  min-height: 200px;
+`;
 
 interface CardProps {
-    character: Char
+  character: Char
 }
 
-const Card = (props: CardProps ) => {
-    return(
-        <div className="card-container">
-            <div className="card-name-container">
-                <h3>{props.character.name}</h3>
-            </div>
-            <div className="card-picture-container">
-                <img src={props.character.picture} alt={props.character.name}/>
-            </div>
-        </div>
-    )
-}
+const Card = (props: CardProps) => {
+  return (
+    <CardContainer
+      container
+      item
+      textAlign="center"
+      display="block"
+      boxShadow={10}
+      mb={2}
+    >
+      <Grid item>
+        <Typography variant="h6" mb={2}>
+          {props.character.name}
+        </Typography>
+      </Grid>
+      <BackgroundImageDiv picture={props.character.picture}>
+      </BackgroundImageDiv>
+    </CardContainer>
+  );
+};
 
-export default Card
+export default Card;
